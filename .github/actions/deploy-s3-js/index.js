8fb20325126273c3c48919d0c46e7933ecb54f48
Mario Salles <mcfsalles@hotmail.com>
2024-03-30 18:27:54 +0000

    deployed on AWS

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -1,8 +1,16 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1) input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+  
+  // 2) upload files
+  exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+
   core.notice('Deploying to S3');
 }
 
