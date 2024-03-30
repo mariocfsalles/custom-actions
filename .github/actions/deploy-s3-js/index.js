661eb7a8e0e8ddb27a1ceef5a47e554bf9beb8b6
Mario Salles <mcfsalles@hotmail.com>
2024-03-30 18:53:25 +0000

    added website aws live

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -4,14 +4,16 @@ const exec = require('@actions/exec');
 
 function run() {
   // 1) input values
+  const distFolder = core.getInput('dist-folder', { required: true });
   const bucket = core.getInput('bucket', { required: true });
   const bucketRegion = core.getInput('bucket-region', { required: true });
-  const distFolder = core.getInput('dist-folder', { required: true });
   
   // 2) upload files
   exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
 
-  core.notice('Deploying to S3');
+  // 3) output values
+  const bucketUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', bucketUrl);
 }
 
 run();
